de6655e96621bf127e3ba0eaef8bf5688aaa46db
Anna Tasheva <anna.tasheva90@gmail.com>
2024-08-02 11:59:20 +0100

    Created all Associations

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -9,7 +9,19 @@ const { Attack } = require("./Attack");
 
 // import the rest of your models above
 
-// set up the associations here
+// set up the Associations here
+
+// One-to-One
+User.hasOne(Deck);
+Deck.belongsTo(User);
+
+// One-to-Many
+Deck.hasMany(Card);
+Card.belongsTo(Deck);
+
+// Many-to-Many
+Card.belongsToMany(Attack, { through: "CardAttacks" });
+Attack.belongsToMany(Card, { through: "CardAttacks" });
 
 // and then export them all below
 module.exports = { User, Deck, Card, Attack };
